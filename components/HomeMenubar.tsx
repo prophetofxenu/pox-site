9385bba942334ebc867b6659c9c54dec03c6f362
William Harrell <wharrell1@protonmail.com>
2021-07-09 22:31:23 -0400

    implemented home menu

---
+++ components/HomeMenubar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "../styles/components/HomeMenubar.module.scss";
+
+import xe from "../public/images/xe.png";
+
+export default function HomeMenubar() {
+  return (
+    <>
+      <div className={styles.homeMenubar} >
+
+        <div className={styles.links} >
+
+          <div className={styles.xeImg}>
+            <Link href="/" passHref ><a>
+              <Image src={xe} alt="Xenon" />
+            </a></Link>
+          </div>
+
+          <div className={styles.textLinks}>
+            <Link href="/blog" >Blog</Link>
+            <Link href="/videos" >Videos</Link>
+            <Link href="/about" >About</Link>
+          </div>
+
+        </div>
+
+      </div>
+
+    </>
+  );
+}
